a29935d146fd6ce4fca085e095e2ae56393c1fb9
AnimeNyaruko <robloxandminecraft1603@gmail.com>
2023-06-30 09:29:12 +0700

    modified:   DataHandler.js

--- DataHandler.js
+++ DataHandler.js
@@ -5,36 +5,16 @@ export async function DataHandler(items, data, date) {
 
     const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
     const response = await fetch(url, {
-        headers: {
-            'Authorization': `token ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    });
-    const responseData = await response.json();
-    console.log(responseData); // Log the response data
-    const fileContents = JSON.parse(atob(responseData.content));
-    fileContents.users.push({
-        "Sản phẩm trúng": items,
-        "Dữ liệu": data,
-        "Thời gian": date
-    });
-    const updatedContent = JSON.stringify(fileContents, null, 2);
-    const encodedContent = btoa(updatedContent);
-    const commitMessage = 'Add new data to the JSON file';
-
-    const updateUrl = `https://api.github.com/repos/${owner}/${repo}/contents/PushData/data.json`;
-    const updateResponse = await fetch(updateUrl, {
-        method: 'PUT',
+        method: 'POST',
         headers: {
             'Authorization': `token ${accessToken}`,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            message: commitMessage,
-            content: encodedContent,
-            sha: responseData.sha
+            title: 'New issue',
+            body: `Sản phẩm trúng: ${items}\nDữ liệu: ${JSON.stringify(data)}\nThời gian: ${date}`
         })
     });
-    const updateData = await updateResponse.json();
-    console.log(updateData);
+    const responseData = await response.json();
+    console.log(responseData);
 }

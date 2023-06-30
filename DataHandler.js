552ac235395df7ad4fd2993ead4651736b1ab498
AnimeNyaruko <robloxandminecraft1603@gmail.com>
2023-06-30 09:42:51 +0700

    modified:   DataHandler.js

--- DataHandler.js
+++ DataHandler.js
@@ -1,5 +1,5 @@
 export async function DataHandler(items, data, date) {
-    const accessToken = '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';
+    const accessToken = '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';
     const owner = 'AnimeNyaruko';
     const repo = 'LuckyWheelWeb';
 
